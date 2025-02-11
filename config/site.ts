b859794d94d211a6bbbdd2d1ca9f2a425153beda
ShimomineSota <128496034+ShimomineSota@users.noreply.github.com>
2025-02-11 12:36:53 +0000

    add: separate config files

---
+++ config/site.ts
@@ -0,0 +1,12 @@
+import type { SiteConfig } from "@/types";
+
+export const siteConfig: SiteConfig = {
+	name: "Post Writer",
+	description: "",
+	url: "localhost:3000",
+	ogImage: "",
+	links: {
+		x: "https://x.com",
+		github: "https://github.com/shimominesota",
+	},
+};
