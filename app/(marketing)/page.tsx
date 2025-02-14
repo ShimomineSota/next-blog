68e503f402415074f336e61c47917cdd283737a6
ShimomineSota <128496034+ShimomineSota@users.noreply.github.com>
2025-02-14 14:42:07 +0000

    add: feature category

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -38,6 +38,101 @@ export default function IndexPage() {
 					</div>
 				</div>
 			</section>
+
+			<section
+				id="features"
+				className="container mx-auto py-8 md:py-12 lg:py-24 bg-stone-50 space-y-6 px-3 md:px-6 lg:px-10"
+			>
+				<div className="text-center space-y-6">
+					<h2 className="font-extrabold text-3xl md:text-6xl">
+						サービスの特徴
+					</h2>
+					<p className="text-muted-foreground sm:text-lg sm:leading-7">
+						マークダウン形式で、ブログの投稿が可能です。
+					</p>
+				</div>
+
+				<div className="mx-auto grid md:grid-cols-2 lg:grid-cols-3 gap-4 max-w-[64rem]:">
+					<div className="bg-background border p-2 rounded-lg">
+						<div className="flex flex-col justify-between p-6 h-[150] lg:h-[160]">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="45"
+								height="45"
+								viewBox="0 0 24 24"
+							>
+								<title>Next.js Icon</title>
+								<path
+									fill="none"
+									stroke="currentColor"
+									strokeLinecap="round"
+									stroke-linejoin="round"
+									stroke-width="2"
+									d="M9 15V9l7.745 10.65A9 9 0 1 1 19 17.657M15 12V9"
+								/>
+							</svg>
+							<div className="space-y-2">
+								<h3 className="font-bold">Next.js</h3>
+								<p className="text-sm text-muted-foreground">
+									AppRouter/Layouts/APIRoutesの技術を使用
+								</p>
+							</div>
+						</div>
+					</div>
+					<div className="bg-background border p-2 rounded-lg">
+						<div className="flex flex-col justify-between p-6 h-[150] lg:h-[160]">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="45"
+								height="45"
+								viewBox="0 0 24 24"
+							>
+								<title>Next.js Icon</title>
+								<path
+									fill="none"
+									stroke="currentColor"
+									strokeLinecap="round"
+									stroke-linejoin="round"
+									stroke-width="2"
+									d="M9 15V9l7.745 10.65A9 9 0 1 1 19 17.657M15 12V9"
+								/>
+							</svg>
+							<div className="space-y-2">
+								<h3 className="font-bold">Next.js</h3>
+								<p className="text-sm text-muted-foreground">
+									AppRouter/Layouts/APIRoutesの技術を使用
+								</p>
+							</div>
+						</div>
+					</div>
+					<div className="bg-background border p-2 rounded-lg">
+						<div className="flex flex-col justify-between p-6 h-[150] lg:h-[160]">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="45"
+								height="45"
+								viewBox="0 0 24 24"
+							>
+								<title>Next.js Icon</title>
+								<path
+									fill="none"
+									stroke="currentColor"
+									strokeLinecap="round"
+									stroke-linejoin="round"
+									stroke-width="2"
+									d="M9 15V9l7.745 10.65A9 9 0 1 1 19 17.657M15 12V9"
+								/>
+							</svg>
+							<div className="space-y-2">
+								<h3 className="font-bold">Next.js</h3>
+								<p className="text-sm text-muted-foreground">
+									AppRouter/Layouts/APIRoutesの技術を使用
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
 		</>
 	);
 }
